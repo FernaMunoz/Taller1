32c1fb315278cf287ad901dad641fcc4fa012e05
HP <f.munoz40@ufromail.cl>
2023-08-24 10:33:49 -0400

    2 commit

--- Matriz.java
+++ Matriz.java
@@ -1,5 +1,5 @@
-import java.security.KeyStore;
 import java.util.Scanner;
+import java.util.Random;
 public class Matriz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
@@ -10,24 +10,40 @@ public class Matriz {
         System.out.print("Ingrese el numero de columnas");
         int COLUMNA = scanner.nextInt();
 
-        System.out.println("Las filas son: "+ FILA + "Las columnas son:" + COLUMNA);
+        System.out.println("Las filas son: " + FILA + " Las columnas son:" + COLUMNA);
 
-        boolean validardimensiones = validarDimensiones(FILA,COLUMNA);
+        int[][] Matriz = new int[FILA][COLUMNA];
+
+        Random random = new Random();
+
+        for (int i = 0; i < FILA; i++) {
+            for (int j = 0; j < COLUMNA; j++) {
+                Matriz[i][j] = random.nextInt(10);
+            }
+        }
+        for (int i = 0; i < FILA; i++) {
+            for (int j = 0; j < COLUMNA; j++) {
+                System.out.print(Matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        boolean validardimensiones = validarDimensiones(FILA, COLUMNA);
 
         if (validardimensiones) {
-            System.out.println("NValido");
+            System.out.println("Numero Valido");
         } else {
-            System.out.println("Invalido");
+            System.out.println("Numero Invalido");
         }
 
     }
 
     public static boolean validarDimensiones(int FILA, int COLUMNA) {
-        if (FILA > 0 && COLUMNA > 0){
+        if (FILA > 0 && COLUMNA > 0) {
             return true;
         } else {
             return false;
-
         }
+
     }
 }
